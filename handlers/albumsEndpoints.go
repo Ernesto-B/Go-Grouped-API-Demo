10714c924cc634180728e2950ebe90edf3f2a9e0
Ernesto-B <ernesto.barreto@ucalgary.ca>
2024-11-20 00:14:54 -0700

    Restructured data.go into models package

--- handlers/albumsEndpoints.go
+++ handlers/albumsEndpoints.go
@@ -3,26 +3,27 @@ package handlers
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	models "go-server-demo/models"
 )
 
 func GetAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	c.IndentedJSON(http.StatusOK, models.Albums)
 }
 
 func PostAlbum(c *gin.Context) {
-	var newAlbum album
+	var newAlbum models.Album
 	err := c.BindJSON(&newAlbum) ; if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
-	albums = append(albums, newAlbum)
+	models.Albums = append(models.Albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 func GetAlbumbyId(c *gin.Context) {
 	param := c.Param("id")
-	// Search for the id in albums
-	for _, val := range albums {
+	// Search for the id in models.Albums
+	for _, val := range models.Albums {
 		if val.ID != param {
 			continue
 		}
